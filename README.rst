6e9aa49d2632c0713ed798bd5885507ebccd4d96
TaTooKa <leandro.rolando@gmail.com>
2023-07-30 15:13:20 -0300

    Refactor all code to new version of RtD.

---
+++ README.rst
@@ -0,0 +1,7 @@
+microFATE
+=========
+
+This is an implementation of the **FATE Core** system by **Evil Hat** (https://evilhat.com/product/fate-core-system/).
+
+This repository contains a *Sphinx based app* prepared for the ReadTheDocs site. 
+The current url to find the **microFATE** rules in readTheDocs format is https://microfate.readthedocs.io/ .
---
+++ docs/source/conf.py
@@ -0,0 +1,48 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'microFATE'
+copyright = '2023, TaTooKa'
+author = 'TaTooKa'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+html_logo = "_static/images/microFATE-logo3.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': True,
+}
+
+def setup(app):
+    app.add_css_file('css/custom.css')
