611668b4128eae330f9cc2a4d5f017acc6e3fe8a
TaTooKa <leandro.rolando@gmail.com>
2023-07-30 13:27:52 -0300

    changed microFATE logo.

--- conf.py
+++ conf.py
@@ -56,7 +56,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = "_static/images/microFATE-logo2.png"
+html_logo = "_static/images/microFATE-logo3.png"
 html_theme_options = {
     'logo_only': True,
     'display_version': True,
